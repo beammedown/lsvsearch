908e369ce1188510e333f00852b00a244456b6ea
beammedown <gaston@liepach.eu>
2025-03-08 16:08:20 -0600

    breaking into smaller files pt1

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:rechtssuche/main.dart';
 import 'package:rechtssuche/pages/index.dart';
 
 void main() {
